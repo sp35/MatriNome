5fda2a0c2ba4c46a077f9d04cfadce01bf6ba3a1
Shivansh Anand <shivansh586@gmail.com>
2019-12-19 17:25:19 +0530

    adding multi choice field

--- users/forms.py
+++ users/forms.py
@@ -3,10 +3,27 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
 
+INTEREST_CHOICES = (
+            ('A', 'travelling'),
+            ('B', 'exercise'),
+            ('C', 'going to the theater'),
+            ('D', 'dancing'),
+            ('E', 'cooking'),
+            ('F', 'doing stuff outdoors'),
+            ('G', 'politics'),
+            ('H', 'pets'),
+            ('I', 'photography'),
+            ('J', 'sports'),
+            ('K', 'art'),
+            ('L', 'learning'),
+            ('M', 'music'),
+            ('N', 'comedy'),
+            ('O', 'reading'),
+        )
 
 class UserRegisterForm(UserCreationForm):
 	email = forms.EmailField()
-
+	
 	class Meta:
 		model = User
 		fields = ['username', 'email', 'password1', 'password2']
