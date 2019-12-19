93ccf74684e2207bf7305c5ffd92ba6e7e54cf93
Shubham Pandey <shubhampcpandey13@gmail.com>
2019-12-19 21:48:07 +0530

    Add intro page

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views as app_views
 
 urlpatterns = [
-    path('', app_views.home, name='home'),
+    path('home', app_views.home, name='home'),
+    path('', app_views.index, name='index')
 ]
--- app/views.py
+++ app/views.py
@@ -62,3 +62,7 @@ from django.template.defaulttags import register
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+
+def index(request):
+    return render(request, 'app/index.html')
