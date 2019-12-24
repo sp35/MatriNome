4a3c8a1080038901f426ade032da844826bb15eb
Shivansh Anand <shivansh586@gmail.com>
2019-12-24 13:57:52 +0530

    deployment changes

--- matrinome/settings.py
+++ matrinome/settings.py
@@ -126,7 +126,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static-deploy')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "static-deploy"),
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
